b17200c334341c1afa273d82ce77a7e5103a10e8
MoondanceZ <Qin_Yikai@outlook.com>
2022-06-08 18:03:29 +0800

    添加项目文件。

---
+++ RedisStreamQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FreeRedis;
+
+Console.WriteLine("Hello, World!");
+
+var cli = new RedisClient("127.0.0.1:6379,password=,defaultDatabase=3");
+cli.Notice += (s, e) => Console.WriteLine(e.Log);
+
+if (!cli.Exists("x-stream"))
+{
+    cli.XGroupCreate("x-stream", "group1", MkStream: true);
+    cli.XGroupCreate("x-stream", "group2", MkStream: true);
+}
+else
+{
+    //多个分组表示消息会分发到多个分组, 消息也会被消费多次
+    var infoGroups = cli.XInfoGroups("x-stream");
+    if (!infoGroups.Any(x => x.name == "group1"))
+        cli.XGroupCreate("x-stream", "group1", MkStream: true);
+
+    //if (!infoGroups.Any(x => x.name == "group2"))
+    //    cli.XGroupCreate("x-stream", "group2", MkStream: true);
+}
+
+var pendingResult = cli.XPending("x-stream", "group1");
+
+var p = cli.XPending("x-stream", "group1", "-", "+", 10);
+
+while (true)
+{
+    var str = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(str))
+    {
+        cli.XAdd("x-stream", "msg", str);
+    }
+}
---
+++ RedisStreamQueueConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FreeRedis;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+var cli = new RedisClient("127.0.0.1:6379,password=,defaultDatabase=3");
+//cli.Notice += (s, e) => Console.WriteLine(e.Log);
+while (true)
+{
+    //多个消费者消费同一个消费组, 消息会随机分布到消费者手上
+    ThreadPool.QueueUserWorkItem(_ =>
+    {
+        var data = cli.XReadGroup("group1", "consumer-1", 5, "x-stream", ">");
+        if (data != null)
+            Console.WriteLine("consumer-1 receive message" + JsonConvert.SerializeObject(data));
+    });
+
+    ThreadPool.QueueUserWorkItem(_ =>
+    {
+        var data = cli.XReadGroup("group1", "consumer-2", 5, "x-stream", ">");
+        if (data != null)
+            Console.WriteLine("consumer-2 receive message" + JsonConvert.SerializeObject(data));
+    });
+}
